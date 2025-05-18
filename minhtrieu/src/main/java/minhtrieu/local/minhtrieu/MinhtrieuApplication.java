fbf3de4bf05ed77ef0e1353de680a27e155f11b5
Triệu Bình Minh <trieuminh2109@gmail.com>
2025-05-18 12:02:48 +0700

    first commit

---
+++ minhtrieu/src/main/java/minhtrieu/local/minhtrieu/MinhtrieuApplication.java
@@ -0,0 +1,13 @@
+package minhtrieu.local.minhtrieu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhtrieuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhtrieuApplication.class, args);
+	}
+
+}
---
+++ minhtrieu/src/test/java/minhtrieu/local/minhtrieu/MinhtrieuApplicationTests.java
@@ -0,0 +1,13 @@
+package minhtrieu.local.minhtrieu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhtrieuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
